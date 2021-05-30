0f3267f0746f39cbe6ae51c7f75556476ab25494
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-05-30 22:57:12 +0530

    Carousel

---
+++ lib/ui/widgets/temp2.dart
@@ -0,0 +1,62 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class Carousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: [
+        //1st Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://www.freepik.com/free-vector/organic-flat-vaccination-campaign_13995230.htm#&position=13"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //2nd Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://www.freepik.com/free-vector/organic-flat-vaccination-campaign_13995230.htm#&position=13"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //3rd Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://media.healthdirect.org.au/images/inline/original/covid-cold-flu-symptoms-bd24a5.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+
+      //Slider Container properties
+      options: CarouselOptions(
+        height: 180.0,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enableInfiniteScroll: true,
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        viewportFraction: 0.8,
+      ),
+    );
+  }
+}
