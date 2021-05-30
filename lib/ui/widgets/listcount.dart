014f529817755e4710708f88085967953a89f3cf
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-05-30 12:39:13 +0530

    Card ui

--- lib/ui/widgets/listcount.dart
+++ lib/ui/widgets/listcount.dart
@@ -2,39 +2,52 @@ import 'package:cowin_vaccine_tracker/models/pincode.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class ListCoutn extends StatelessWidget {
   final Centers centers;
 
   const ListCoutn({Key key, this.centers}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Text(centers.name),
-          Text(centers.sessions[0].vaccine),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25), // if you need this
+          side: BorderSide(
+            color: Colors.grey.withOpacity(0.3),
+            width: 1,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
             children: [
-              SubPart(
-                text1: "Date",
-                text2: "Nov25",
-                color: Colors.black,
-              ),
-              SubPart(
-                text1: "Time",
-                text2: "3:30-4:30",
-                color: Colors.black,
-              ),
-              SubPart(
-                text1: "Paid",
-                text2: "Free",
-                color: Colors.red,
-              ),
+              Text(centers.name),
+              Text(centers.sessions[0].vaccine),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SubPart(
+                    text1: "Date",
+                    text2: "Nov25",
+                    color: Colors.black,
+                  ),
+                  SubPart(
+                    text1: "Time",
+                    text2: "3:30-4:30",
+                    color: Colors.black,
+                  ),
+                  SubPart(
+                    text1: "Paid",
+                    text2: "Free",
+                    color: Colors.red,
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
