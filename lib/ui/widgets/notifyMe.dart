4d843e58d0e7285f00cea13219c33c4ffd4982df
flutterStak <vivekdusad55@gmail.com>
2021-06-03 18:13:26 +0530

    "bacground Functanality Added"

---
+++ lib/ui/widgets/notifyMe.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class NotifyMe extends StatelessWidget {
+  const NotifyMe({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
